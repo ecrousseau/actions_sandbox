b5029a3e64a320e106b21bb5fac1409ece3ecc7f
Edward Rousseau <erousseau@deloitte.com.au>
2020-10-12 17:19:21 +1100

    pages?

--- script.py
+++ script.py
@@ -3,15 +3,11 @@ import os
 print("# terraform-aws-ec2-instance\n")
 print("## Files and Folders\n")
 for (dirpath, dirnames, filenames) in os.walk(f"{os.getcwd()}/terraform-aws-ec2-instance"):
+    dirnames[:] = [d for d in dirnames if not d.split('/')[-1].startswith('.')]
     print(f"### {dirpath}\n")
-    for filename in filenames:
-        if (not filename.startswith('.')) and filename.endswith('.tf'):
-            print(f"* **{filename}**\n")
-        elif not filename.startswith('.'):
-            print(f"* {filename}\n")
+    for f in filenames:
+        if (not f.startswith('.')) and f.endswith('.tf'):
+            print(f"* **{f}**\n")
+        elif not f.startswith('.'):
+            print(f"* {f}\n")
     print("\n")
-    _dirnames = []
-    for dirname in dirnames:
-        if not dirname.startswith('.'):
-            _dirnames.append(dirname)
-    dirnames = _dirnames
